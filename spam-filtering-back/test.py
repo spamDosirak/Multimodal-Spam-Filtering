cf07b3e418e2933fee07516e0cfff85035c950a9
HyeGyoungPark <hyegp@gmail.com>
2023-09-13 19:08:56 +0900

    test.py 파일이 수정 전으로 올라가서 다시 올립니다.

--- spam-filtering-back/test.py
+++ spam-filtering-back/test.py
@@ -21,11 +21,21 @@ import io
 import wave
 import re
 import sys
-
 from google.cloud import speech
-
 import pyaudio
 from six.moves import queue
+## image caption
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Model
+from tensorflow.keras.models import load_model
+
+
+
 
 
 
@@ -75,7 +85,111 @@ def SVM():
 
 
 #Image -> Text 후 Spam 검사
+##Image caption 추출
+def image_caption(img_name):
+	all_captions = joblib.load('all_captions.pkl')
 
+	# get maximum length of the caption available
+	max_length = max(len(caption.split()) for caption in all_captions)
+
+	# tokenize the text
+	tokenizer = Tokenizer()
+	tokenizer.fit_on_texts(all_captions)
+
+	# load features from pickle
+	features = joblib.load('image_caption_features_new2.pkl')
+
+	# load the model
+	img_cap_model = load_model('best_image_caption_model_new2.h5')
+
+	def idx_to_word(integer, tokenizer):
+		for word, index in tokenizer.word_index.items():
+			if index == integer:
+				return word
+		return None
+
+	# generate caption for an image
+	def predict_caption(model, image, tokenizer, max_length):
+		# add start tag for generation process
+		in_text = 'startseq'
+		# iterate over the max length of sequence
+		for i in range(max_length):
+			# encode input sequence
+			sequence = tokenizer.texts_to_sequences([in_text])[0]
+			# pad the sequence
+			sequence = pad_sequences([sequence], max_length)
+			# predict next word
+			yhat = model.predict([image, sequence], verbose=0)
+			# get index with high probability
+			yhat = np.argmax(yhat[0])
+			# convert index to word
+			word = idx_to_word(yhat, tokenizer)
+			# stop if word not found
+			if word is None:
+				break
+			# append word as input for generating next word
+			in_text += " " + word
+			# stop if we reach end tag
+			if word == 'endseq':
+				break
+		return in_text
+
+
+	##기존에 입력된 적 없는 이미지에서의 추출 위한 모델
+	vgg_model = VGG16() 
+	# restructure the model
+	vgg_model = Model(inputs=vgg_model.inputs,             
+					outputs=vgg_model.layers[-2].output)
+
+	#image 에서 feature 추출
+	def extract_features_from_image(model, img_path):
+		# load the image from file
+		image = load_img(img_path, target_size=(224, 224))
+		# convert image pixels to numpy array
+		image = img_to_array(image)
+		# reshape data for model
+		image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+		# preprocess image for vgg
+		image = preprocess_input(image)
+		# extract features
+		feature = model.predict(image, verbose=0)
+		return feature
+
+
+	from PIL import Image
+	import matplotlib.pyplot as plt
+	def generate_caption(model,image_name):
+		# load the image
+		image_id = image_name.split('.')[0]
+		img_path = os.path.join(image_name)
+		image = Image.open(img_path)
+
+		# Check if we already have features for this image
+		if image_id not in features:
+			print("Extracting features for the image...")
+			# If not, use VGG16 model to extract the features
+			features[image_id] = extract_features_from_image(vgg_model, img_path)
+		
+		# predict the caption
+		y_pred = predict_caption(model, features[image_id], tokenizer, max_length)
+		return(list(y_pred.split(' ')))
+
+	def clean_pred(gen_cap):
+		prev = ""
+		fin_pred = ""
+		for i in range(1,len(gen_cap)-1):
+			prev = gen_cap[i-1]
+			if(gen_cap[i] == prev):
+				None
+			else:
+				fin_pred += gen_cap[i]+' '
+		fin_pred += '.'
+		return fin_pred
+	return clean_pred(generate_caption(img_cap_model, img_name))
+
+
+
+##Image에 있는 Text 추출 후 Image caption과 데이터 합치기
 @app.route('/convert/image', methods=['POST'])
 def convertImage():
 	image = request.files['image']
@@ -84,6 +198,7 @@ def convertImage():
 	image_file = filepath
 
 	print(image_file)
+	img_caption = image_caption(image_file)
 
 	# 이미지 처리 로직 작성
 	api_url = 'https://ywfsh0h15b.apigw.ntruss.com/custom/v1/23768/4747a73a47d3b6a70bebaaa85c450d91240ccbefdf1363ce9a10bcef1c6534e1/general'
@@ -118,7 +233,7 @@ def convertImage():
 	for field in result['images'][0]['fields']:
 		text += field['inferText'] + ' '
 	#print(text)
-	res = total_predict(text)
+	res = total_predict(img_caption+text)
 	
 	return res
 
